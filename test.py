fe3325232888bcef9df9a526914213ae81aaa52c
Jinghan Zhou <zhoujh15@mails.tsinghua.edu.cn>
2018-04-22 21:48:23 +0800

    basic part finished

---
+++ test.py
@@ -0,0 +1,130 @@
+import cv2 as cv  
+import numpy as np
+
+input_url = '/Users/mac/Desktop/university/CST/1718Spring/6_数字图像处理/hw/1/git/Digital_Image_Processing_Ex1/src/input'
+output_url = '/Users/mac/Desktop/university/CST/1718Spring/6_数字图像处理/hw/1/git/Digital_Image_Processing_Ex1/src/output'
+
+# image = cv.imread('/Users/mac/Desktop/university/CST/1718Spring/6_数字图像处理/hw/1/第一次大作业/face morphing/cruz.png')
+# cv.namedWindow("Image") # 初始化一个名为Image的窗口
+# cv.imshow("Image", image) # 显示图片
+
+# print(image[0][0])
+
+# cv.waitKey(0) # 等待键盘触发事件，释放窗口
+
+def brightness(inputimage, g = 5):
+    outputimage = inputimage.copy()
+    for i in range(0, len(inputimage)):
+        for j in range(0, len(inputimage[0])):
+            for k in range(0, 3):
+                if inputimage[i][j][k] + g < 256 and inputimage[i][j][k] + g >= 0:
+                    outputimage[i][j][k] = inputimage[i][j][k] + g
+                elif inputimage[i][j][k] + g > 255:
+                    outputimage[i][j][k] = 255
+                elif inputimage[i][j][k] + g < 0:
+                    outputimage[i][j][k] = 0
+    return outputimage
+
+def contrast(inputimage, a):
+    outputimage = inputimage.copy()
+    if a < 0:
+        print('wrong number!!!\n')
+        return outputimage
+    else:
+        for i in range(0, len(inputimage)):
+            for j in range(0, len(inputimage[0])):
+                for k in range(0, 3):
+                    tmp_color = a * (inputimage[i][j][k] - 127) + 127
+                    if tmp_color < 0:
+                        outputimage[i][j][k] = 0
+                    elif tmp_color >= 0 and tmp_color <= 255:
+                        outputimage[i][j][k] = tmp_color
+                    elif tmp_color > 255:
+                        outputimage[i][j][k] = 255
+    return outputimage
+
+def gamma(inputimage, r):
+    outputimage = inputimage.copy()
+    if r < 0:
+        print('wrong number!!!\n')
+        return outputimage
+    else:
+        for i in range(0, len(inputimage)):
+            for j in range(0, len(inputimage[0])):
+                for k in range(0, 3):
+                    outputimage[i][j][k] = 255 * (inputimage[i][j][k] / 255) ** (1 / r)
+    return outputimage
+
+def histogram_equalization(inputimage):
+    outputimage = inputimage.copy()
+    total_size = len(inputimage) * len(inputimage[0])
+    histo = {}#total
+    for i in range(0, 256):
+        histo[i] = 0
+    for i in range(0, len(inputimage)):
+        for j in range(0, len(inputimage[0])):
+            histo[int(inputimage[i][j].sum() / 3)] += 1
+            #histo[int((inputimage[i][j][2] * 0.299 + inputimage[i][j][1] * 0.587 + inputimage[i][j][0] * 0.114) / 3)] += 1
+    tmp = 0
+    for j in range(0, 256):
+        tmp += histo[j]
+        histo[j] = tmp 
+    # building the histo
+    for i in range(0, len(inputimage)):
+        for j in range(0, len(inputimage[0])):
+            for k in range(0, 3):
+                outputimage[i][j][k] = 255 * (histo[inputimage[i][j][k]] / total_size)
+    return outputimage
+
+def histogram_matching(inputimage, matchingimage):
+    outputimage = inputimage.copy()
+    total_size = len(matchingimage) * len(matchingimage[0])
+    histo = {}#total
+    for i in range(0, 256):
+        histo[i] = 0
+    for i in range(0, len(matchingimage)):
+        for j in range(0, len(matchingimage[0])):
+            histo[int(matchingimage[i][j].sum() / 3)] += 1
+    tmp = 0
+    for j in range(0, 256):
+        tmp += histo[j]
+        histo[j] = tmp
+    for i in range(0, len(inputimage)):
+        for j in range(0, len(inputimage[0])):
+            for k in range(0, 3):
+                outputimage[i][j][k] = 255 * (histo[inputimage[i][j][k]] / total_size)
+    return outputimage
+
+def saturation(inputimage):
+    #???
+    return 0
+
+def point_processing():
+    # image1 = cv.imread(input_url + '/4.jpg')
+    # output_brigtness1 = brightness(image1, 50)
+    # cv.imwrite(output_url + '/4_output_brigtness1.jpg', output_brigtness1)
+    # output_brigtness2 = brightness(image1, -50)
+    # cv.imwrite(output_url + '/4_output_brigtness2.jpg', output_brigtness2)
+    # output_contrast1 = contrast(image1, 0.75)
+    # cv.imwrite(output_url + '/4_output_contrast1.jpg', output_contrast1)
+    # output_contrast2 = contrast(image1, 1.25)
+    # cv.imwrite(output_url + '/4_output_contrast2.jpg', output_contrast2)
+    # output_gamma1 = gamma(image1, 0.75)
+    # cv.imwrite(output_url + '/4_output_gamma1.jpg', output_gamma1)
+    # output_gamma2 = gamma(image1, 1.25)
+    # cv.imwrite(output_url + '/4_output_gamma2.jpg', output_gamma2)
+    # output_histogram_equalization = histogram_equalization(image1)
+    # cv.imwrite(output_url + '/4_output_histogram_equalization.jpg', output_histogram_equalization)
+    # image2 = cv.imread(input_url + '/5.jpg')
+    # output_histogram_matching = histogram_matching(image1, image2)
+    # cv.imwrite(output_url + '/4_output_histogram_matching.jpg', output_histogram_matching)
+    # ~~~~~~~~~~~~
+    # cv.namedWindow('output1')
+    # cv.imshow('output1', output_brigtness2)
+    # cv.waitKey(0)
+    return 0
+
+point_processing()
+
+
+
